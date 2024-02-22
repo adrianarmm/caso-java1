75963564551c6ab032e4fbc30b6a7f9065e2323f
adrianarmm <areyemor@myuax.com>
2024-02-22 12:35:12 +0100

    26

--- src/main/java/SistemaMonitoreo.java
+++ src/main/java/SistemaMonitoreo.java
@@ -2,5 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SistemaMonitoreo {
+    public static void main(String[] args) {
 
+        monitorearRadiacion(5.2); // Valor seguro
+        monitorearRadiacion(15.6); // Valor extremo
+
+        monitorearTemperatura(22); // Valor seguro
+        monitorearTemperatura(-5); // Valor extremo
+
+        monitorearPresion(101.325); // Valor seguro
+        monitorearPresion(50.5); // Valor extremo
 }
