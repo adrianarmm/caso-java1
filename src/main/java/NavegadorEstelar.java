82f40beda57b8939137c69d6d62394e28858662f
adrianarmm <areyemor@myuax.com>
2024-02-22 12:49:47 +0100

    43

---
+++ src/main/java/NavegadorEstelar.java
@@ -0,0 +1,10 @@
+public class NavegadorEstelar {
+    public static void main(String[] args) {
+        // Representación simplificada del terreno como una matriz
+        int[][] terreno = {
+                {1, 1, 0, 2},
+                {1, 0, 0, 1},
+                {2, 0, 1, 1},
+                {1, 1, 1, 0}
+        };
+}
