c0b844d5d9bec92dd585efd40c2c8df9165cb36f
adrianarmm <areyemor@myuax.com>
2024-02-22 11:54:55 +0100

    5

--- src/main/java/RecursosySuministros.java
+++ src/main/java/RecursosySuministros.java
@@ -5,4 +5,11 @@ public class RecursosySuministros {
     // Recursos y suministros para una expedición espacial
         private static final double CONSUMO_AGUA_DIA = 1.5; // 1.5 litros
         private static final double CONSUMO_COMIDA_DIA = 0.5; // 0.5 kg
+        private static final double CONSUMO_OXIGENO_DIA = 0.84; // 0.84 m^3 de oxígeno
+
+
+    private static final double UMBRAL_SEGURO = 50; // Porcentaje mínimo seguro de recursos
+
+    public static void main(String[] args) {
+        int dias = 5; // Ejemplo de múltiplo de 5 días
 }
