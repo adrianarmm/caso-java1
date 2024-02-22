1b18ccf725225d16b188dd8353bc0c4612d593f5
adrianarmm <areyemor@myuax.com>
2024-02-22 11:53:26 +0100

    4

---
+++ src/main/java/RecursosySuministros.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecursosySuministros {
+    // Recursos y suministros para una expedición espacial
+        private static final double CONSUMO_AGUA_DIA = 1.5; // 1.5 litros
+        private static final double CONSUMO_COMIDA_DIA = 0.5; // 0.5 kg
+}
