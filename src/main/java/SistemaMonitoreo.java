302de7eb25cb29524b1e28107856e51c29255c14
adrianarmm <areyemor@myuax.com>
2024-02-22 12:36:36 +0100

    28

--- src/main/java/SistemaMonitoreo.java
+++ src/main/java/SistemaMonitoreo.java
@@ -14,4 +14,15 @@ public class SistemaMonitoreo {
         monitorearPresion(50.5); // Valor extremo
 
         listarPrimerosNPrimos(5); // Listar los primeros 5 números primos
+
+        System.out.println("Factores primos de 60: " + descomponerEnFactoresPrimos(60));
 }
+    private static void monitorearRadiacion(double nivelRadiacion) {
+        if (nivelRadiacion > 10) { // Supongamos que 10 es el máximo seguro
+            System.out.println("Alerta: Nivel de radiación crítico. Sugerencia: Incrementar el aislamiento.");
+        } else {
+            System.out.println("Nivel de radiación seguro.");
+        }
+    }
+
+
