0d257b60ce636b5480318b4f9c4706e087d322e6
adrianarmm <areyemor@myuax.com>
2024-02-22 12:37:53 +0100

    32

--- src/main/java/SistemaMonitoreo.java
+++ src/main/java/SistemaMonitoreo.java
@@ -52,3 +52,13 @@ public class SistemaMonitoreo {
             numero++;
         }
     }
+
+    private static boolean esPrimo(int numero) {
+        for (int i = 2; i <= numero / 2; i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
