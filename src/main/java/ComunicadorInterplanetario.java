f948645ffdd8252703c92fff344adbc8b197f881
adrianarmm <areyemor@myuax.com>
2024-02-23 09:35:49 +0100

    56

--- src/main/java/ComunicadorInterplanetario.java
+++ src/main/java/ComunicadorInterplanetario.java
@@ -1,4 +1,10 @@
 public class ComunicadorInterplanetario {
     public static void main(String[] args) {
         String mensaje = "La Tierra es planeta más denso del Sistema Solar.";
+
+        System.out.println("Mensaje Original: " + mensaje);
+        System.out.println("Cantidad de vocales: " + contarVocales(mensaje));
+        System.out.println("Mensaje Invertido: " + invertirMensaje(mensaje));
+        System.out.println("¿Es Palíndromo?: " + esPalindromo(mensaje));
+    }
 }
