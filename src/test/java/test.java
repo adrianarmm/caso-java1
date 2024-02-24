fcda6683de2d8bec0b695fd6639e4282a72fe272
adrianarmm <areyemor@myuax.com>
2024-02-24 18:56:46 +0100

    82

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,1 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CronometroCosmicoTest {
+
+    @Test
+    void testCalcularSegundosEnUnAñoTerrestre() {
+        assertEquals(31536000, CronometroCosmico.calcularSegundosEnUnAñoTerrestre());
+    }
+
+    @Test
+    void testCalcularAñosPlaneta() {
+        // Considerando la precisión en los cálculos de punto flotante
+        assertEquals(0.531, CronometroCosmico.calcularAñosPlaneta(365), 0.001);
+    }
+}
 
