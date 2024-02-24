462a7abcd143e793f67cc1d8d4b38bbd19dd6e13
adrianarmm <areyemor@myuax.com>
2024-02-24 13:29:17 +0100

    74

--- src/test/java/conjunto.java
+++ src/test/java/conjunto.java
@@ -1,3 +1,18 @@
 
-
+import java.util.Scanner;
 public class conjunto {
+    Scanner scanner = new Scanner(System.in);
+        while (true) {
+        System.out.println("\nBienvenido al Software Integral de la Misión Espacial");
+        System.out.println("1. Cronómetro Cósmico");
+        System.out.println("2. Recursos y Suministros");
+        System.out.println("3. Sistema de Monitoreo");
+        System.out.println("4. Planificador de Tareas");
+        System.out.println("5. Navegador Estelar");
+        System.out.println("6. Comunicador Interplanetario");
+        System.out.println("7. Salir");
+        System.out.print("Seleccione una opción: ");
+        int opcion = scanner.nextInt();
+
+
+}
